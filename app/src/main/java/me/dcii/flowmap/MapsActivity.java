9c00819bcdd981d9966e206fbc2e8473101e2693
Cinfwat Dogak <cdogakz@yahoo.com>
2017-09-21 13:53:01 +0100

    Enable current user location to be shown on the map

--- app/src/main/java/me/dcii/flowmap/MapsActivity.java
+++ app/src/main/java/me/dcii/flowmap/MapsActivity.java
@@ -1,17 +1,32 @@
 package me.dcii.flowmap;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int REQUEST_CODE_LOCATION = 1;
     private GoogleMap mMap;
 
     @Override
@@ -37,10 +52,116 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        initialiseMap();
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void initialiseMap() {
+        if (checkLocationPermission()) {
+            LocationManager locationManager = (LocationManager)
+                    getSystemService(Context.LOCATION_SERVICE);
+            Location location = null;
+
+            if (locationManager != null) {
+                final Criteria criteria = new Criteria();
+
+                location = locationManager
+                        .getLastKnownLocation(locationManager
+                                .getBestProvider(criteria, false));
+            }
+
+            if (location != null) {
+                final LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                final MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+
+                final int zoom = 13;
+                mMap.addMarker(markerOptions);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+                final CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(latLng)
+                        .zoom(15)
+                        .bearing(90)
+                        .tilt(40)
+                        .build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        }
+    }
+
+    private boolean checkLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED) {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    showRationaleDialog();
+                } else {
+                    requestLocationPermission();
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                REQUEST_CODE_LOCATION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initialiseMap();
+                } else {
+//                    TODO: Disable modules depending on this permission
+                }
+            }
+        }
+    }
+
+    public static class LocationRationaleDialogFragment extends DialogFragment {
+
+        public static LocationRationaleDialogFragment newInstance() {
+            return new LocationRationaleDialogFragment();
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            setCancelable(false);
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle(getString(R.string.location_request_rationale_title,
+                            getString(R.string.app_name)))
+                    .setMessage(getString(
+                            R.string.location_request_rationale, getString(R.string.app_name)))
+                    .setPositiveButton(R.string.alert_dialog_ok,
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    ((MapsActivity) getActivity()).doPositiveClick();
+                                }
+                            }
+                    )
+                    .create();
+        }
+    }
+
+    private void showRationaleDialog() {
+        final DialogFragment newFragment = LocationRationaleDialogFragment.newInstance();
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.location_request_rationale));
+    }
+
+    private void doPositiveClick() {
+        requestLocationPermission();
     }
 }
